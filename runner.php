6dc78e7759a033d211a4e5d9a1caa67ccf6d1f3b
Eirik S. Morland <eirik@morland.no>
2018-05-05 16:39:01 +0200

    Start using new cosy without app token

--- runner.php
+++ runner.php
@@ -1,38 +1,59 @@
 <?php
+
+/**
+ * @file
+ * Runner.
+ *
+ * @author eiriksm <eirik@morland.no>
+ */
+
 use eiriksm\CosyComposer\Message;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 require_once "vendor/autoload.php";
 
-$token = $_SERVER['token'];
 $slug = $_SERVER['slug'];
 $user_token = $_SERVER['user_token'];
 $fork_to = $_SERVER['fork_to'];
 $fork_user = $_SERVER['fork_user'];
 $fork_mail = $_SERVER['fork_mail'];
+$token_url = $_SERVER['token_url'];
+$project = null;
+if (!empty($_SERVER['project'])) {
+    $project = @unserialize(@json_decode($_SERVER['project']));
+}
 $container = new ContainerBuilder();
 $container->register('app', \Composer\Console\Application::class);
 $container->register('output', \eiriksm\ArrayOutput\ArrayOutput::class);
 $container->register('logger', 'Wa72\SimpleLogger\ArrayLogger');
 $container->register('process.factory', 'eiriksm\CosyComposer\ProcessFactory');
 $container->register('command', 'eiriksm\CosyComposer\CommandExecuter')
-  ->addArgument(new Reference('logger'))
-  ->addArgument(new Reference('process.factory'));
+    ->addArgument(new Reference('logger'))
+    ->addArgument(new Reference('process.factory'));
 $container->register('cosy', 'eiriksm\CosyComposer\CosyComposer')
-    ->addArgument($token)
     ->addArgument($slug)
     ->addArgument(new Reference('app'))
     ->addArgument(new Reference('output'))
     ->addArgument(new Reference('command'))
     ->addMethodCall('setLogger', [new Reference('logger')]);
 
-/** @var \eiriksm\CosyComposer\CosyComposer $cosy */
+/* @var \eiriksm\CosyComposer\CosyComposer $cosy */
 $cosy = $container->get('cosy');
 $cosy->setGithubAuth($user_token, 'x-oauth-basic');
 $cosy->setForkUser($fork_to);
-$cosy->setGithubForkAuth($fork_user, $token, $fork_mail);
-$cosy->setTmpDir(sprintf('/tmp/violinist-%d-%s-%s', 8, date('Y.m.d-H.i.s', time()), uniqid()));
+$cosy->setProject($project);
+$cosy->setGithubForkAuth($fork_user, $fork_mail);
+$cosy->setTokenUrl($token_url);
+$cosy
+    ->setTmpDir(
+        sprintf(
+            '/tmp/violinist-%d-%s-%s',
+            8,
+            date('Y.m.d-H.i.s', time()),
+            uniqid()
+        )
+    );
 $cache_dir = $_SERVER['HOME'] . '/.cosy-cache';
 if (!file_exists($cache_dir)) {
     mkdir($cache_dir);
@@ -40,25 +61,25 @@ if (!file_exists($cache_dir)) {
 $cosy->setCacheDir($cache_dir);
 $code = 0;
 try {
-  $cosy->run();
-  $output = $cosy->getOutput();
+    $cosy->run();
+    $output = $cosy->getOutput();
 }
 catch (Exception $e) {
-  $output = $cosy->getOutput();
-  $output[] = new Message('Caught Exception: ' . $e->getMessage());
-  $code = 1;
+    $output = $cosy->getOutput();
+    $output[] = new Message('Caught Exception: ' . $e->getMessage());
+    $code = 1;
 }
 $json = [];
 foreach ($output as $type => $message) {
-  if (empty($message)) {
-    continue;
-  }
-  $json[] = [
+    if (empty($message)) {
+        continue;
+    }
+    $json[] = [
       'message' => $message->getMessage(),
       'context' => $message->getContext(),
       'timestamp' => $message->getTimestamp(),
       'type' => $message->getType(),
-  ];
+    ];
 }
 print json_encode($json);
 exit($code);
