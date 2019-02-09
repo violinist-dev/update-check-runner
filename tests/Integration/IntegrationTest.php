7b7e5f5a0dd1822eb5b8a970561967016f4f4084
Eirik S. Morland <eirik@morland.no>
2019-02-09 17:54:27 +0100

    Start of tests

---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Violinist\UpdateCheckRunner\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+use Violinist\Slug\Slug;
+
+class IntegrationTest extends TestCase
+{
+    /**
+     * This is so not the way phpunit is supposed to be used.
+     */
+    public function testOutput()
+    {
+        $process = new Process(sprintf(
+            'docker run -i --rm -e user_token=%s -e project_url=%s update-check-runner',
+            getenv('user_token'),
+            getenv('project_url')
+        ), null, null, null, 600);
+        $process->run();
+        $this->assertEquals(0, $process->getExitCode());
+        $json = @json_decode($process->getOutput());
+        $this->assertEquals(sprintf('Starting update check for %s', Slug::createFromUrl(getenv('project_url'))->getSlug()), $json[0]->message);
+    }
+}
