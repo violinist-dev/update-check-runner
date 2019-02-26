992201cc2fd3dd3ac1145eb8360dff9cf1210750
Eirik S. Morland <eirik@morland.no>
2019-02-26 18:40:22 +0100

    Make sure we use short hash of the travis commit

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -39,7 +39,7 @@ class IntegrationTest extends TestCase
 
     protected function assertHashLogged($json)
     {
-        $expected_message = sprintf('Queue runner revision %s', getenv('TRAVIS_COMMIT'));
+        $expected_message = sprintf('Queue runner revision %s', substr(getenv('TRAVIS_COMMIT'), 0, 7));
         $this->findMessage($expected_message, $json);
     }
 
