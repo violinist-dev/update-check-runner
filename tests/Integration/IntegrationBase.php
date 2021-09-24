14d599cbf71cd778e7631fef9954a0ec67e213e4
Eirik S. Morland <eirik@morland.no>
2021-09-24 10:06:41 +0200

    Refactor to base test class

---
+++ tests/Integration/IntegrationBase.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Violinist\UpdateCheckRunner\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Process\Process;
+use Violinist\Slug\Slug;
+
+abstract class IntegrationBase extends TestCase
+{
+
+    public function setUp()
+    {
+        try {
+            $env = new Dotenv();
+            $env->load(__DIR__ . '/../../.env');
+        } catch (\Throwable $e) {
+            // We tried.
+        }
+    }
+
+
+    protected function assertStandardOutput($url, $json)
+    {
+        $this->assertHashLogged($json);
+        $this->assertPhpVersionLogged($json);
+        $this->assertProjectStarting($url, $json);
+        $this->assertRepoCloned($json);
+        $this->assertComposerInstalled($json);
+        $this->assertComposerVersion($json);
+    }
+
+    protected function assertComposerVersion($json)
+    {
+        $expected_message = sprintf('Composer %d', getenv('COMPOSER_VERSION'));
+        foreach ($json as $item) {
+            if (strpos($item->message, $expected_message) === 0) {
+                return;
+            }
+        }
+        $this->assertTrue(false, 'The message ' . $expected_message . ' was not found in the output.');
+    }
+
+    protected function assertPhpVersionLogged($json)
+    {
+        foreach ($json as $item) {
+            if (preg_match('/^PHP \d.\d.\d/', $item->message)) {
+                return;
+            }
+        }
+        $this->assertTrue(false, 'The php version was not found in the output');
+    }
+
+    protected function assertHashLogged($json)
+    {
+        $expected_message = sprintf('Queue runner revision %s', substr(getenv('MY_COMMIT'), 0, 7));
+        foreach ($json as $item) {
+            if ($item->message === $expected_message) {
+                return;
+            }
+        }
+        // So it was not found. Let's look at what we actually have.
+        foreach ($json as $item) {
+            if (strpos($item->message, 'Queue runner revision') === 0) {
+                print_r("POSSIBLE HASH MSG: " . $item->message . "\n");
+            }
+        }
+        $this->assertTrue(false, 'The message ' . $expected_message . ' was not found in the output.');
+    }
+
+    protected function findMessage($message, $json)
+    {
+        foreach ($json as $item) {
+            if ($item->message === $message) {
+                return $item;
+            }
+        }
+        $this->assertTrue(false, 'The message ' . $message . ' was not found in the output.');
+    }
+
+    protected function assertProjectStarting($url, $json)
+    {
+        $this->findMessage(sprintf('Starting update check for %s', Slug::createFromUrl($url)->getSlug()), $json);
+    }
+
+    protected function assertRepoCloned($json)
+    {
+        $this->findMessage('Repository cloned', $json);
+    }
+
+    protected function assertComposerInstalled($json)
+    {
+        $this->findMessage('composer install completed successfully', $json);
+    }
+
+    protected function getProcessAndRunWithoutError($token, $url, $other_env = [])
+    {
+        $env_part = sprintf('-e user_token=%s -e project_url=%s', $token, $url);
+        foreach ($other_env as $var => $value) {
+            $env_part .= sprintf(' -e %s=%s', $var, $value);
+        }
+        $process = new Process(sprintf(
+            'docker run -i --rm %s update-check-runner',
+            $env_part
+        ), null, null, null, 600);
+        $process->run();
+        if ($process->getExitCode()) {
+            var_export($process->getOutput());
+        }
+        $this->assertEquals(0, $process->getExitCode(), 'Docker did not exit with exit code 0');
+        $json = @json_decode($process->getOutput());
+        $this->assertFalse(empty($json));
+        return $json;
+    }
+
+}
