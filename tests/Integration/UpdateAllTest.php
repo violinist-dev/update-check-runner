14fce553db8416859f4c141e533f328208de655a
Eirik Stanghelle Morland <eirik@morland.no>
2022-02-16 13:50:19 +0100

    Support always update all (#307)

---
+++ tests/Integration/UpdateAllTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Violinist\UpdateCheckRunner\Tests\Integration;
+
+class UpdateAllTest extends IntegrationBase
+{
+
+    public function testUpdateAll()
+    {
+        $json = $this->getProcessAndRunWithoutError(getenv('GITHUB_PRIVATE_USER_TOKEN'), getenv('GITHUB_PRIVATE_UPDATE_ALL_REPO'), [
+            'fork_to' => getenv('GITHUB_FORK_TO'),
+            'fork_user' => getenv('FORK_USER'),
+            'fork_mail' => getenv('FORK_MAIL'),
+        ]);
+        $found_update_all_type = false;
+        foreach ($json as $value) {
+            if ($value->message !== 'Config suggested update type update_all') {
+                continue;
+            }
+            $found_update_all_type = true;
+        }
+        self::assertTrue($found_update_all_type);
+    }
+
+}
