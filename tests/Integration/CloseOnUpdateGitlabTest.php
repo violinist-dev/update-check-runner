527d8015e2caba3c6dc4dd4e1e7361d901b3b809
Eirik S. Morland <eirik@morland.no>
2022-07-04 20:48:36 +0200

    Add a gitlab has closed things test

---
+++ tests/Integration/CloseOnUpdateGitlabTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Violinist\UpdateCheckRunner\Tests\Integration;
+
+use Bitbucket\Client;
+use Bitbucket\HttpClient\Message\FileResource;
+use Gitlab\Client as GitlabClient;
+use Gitlab\ResultPager;
+use Stevenmaguire\OAuth2\Client\Provider\Bitbucket;
+use Violinist\Slug\Slug;
+
+class CloseOnUpdateGitlabTest extends CloseOnUpdateBase
+{
+    public function testPrsClosedGitlab(&$retries = 0)
+    {
+        $url = getenv('GITLAB_PRIVATE_REPO');
+        $token = $this->getGitlabToken($url);
+        // We want to just, I guess, open all the PRs again?
+        $client = new GitlabClient();
+        $client->authenticate($token, GitlabClient::AUTH_OAUTH_TOKEN);
+        $pager = new ResultPager($client);
+        $api = $client->api('mr');
+        $method = 'all';
+        $url_parsed = parse_url($url);
+        $project_id = ltrim($url_parsed['path'], '/');
+        $prs = $pager->fetchAll($api, $method, [$project_id]);
+        foreach ($prs as $pr) {
+            if ($pr['state'] == 'opened') {
+                continue;
+            }
+            try {
+                $client->mergeRequests()->update($project_id, $pr['iid'], [
+                    'state_event' => 'reopen',
+                ]);
+            } catch (\Throwable $e) {
+                // Meh, what can you do.
+            }
+        }
+        // Now, let's run this stuff. It should certainly contain some closing action.
+        $extra_params = [
+            'fork_user' => getenv('FORK_USER'),
+            'fork_mail' => getenv('FORK_MAIL'),
+        ];
+        $json = $this->getProcessAndRunWithoutError($token, $url, $extra_params);
+        $has_it = self::hasPrClosedAndPrClosedSuccess($json);
+        if ($retries < 20 && !$has_it) {
+            $retries++;
+            return $this->testPrsClosedGitlab($retries);
+        }
+        self::assertTrue($has_it);
+    }
+}
