f3b55031e0fca0e15eccd9331d8701b3bdce843f
Eirik S. Morland <eirik@morland.no>
2022-07-09 22:24:07 +0200

    Fix github tests

--- tests/Integration/CloseOnUpdateGithubPublicTest.php
+++ tests/Integration/CloseOnUpdateGithubPublicTest.php
@@ -6,26 +6,30 @@ use Stevenmaguire\OAuth2\Client\Provider\Bitbucket;
 use Violinist\ProjectData\ProjectData;
 use Violinist\Slug\Slug;
 
-class CloseOnUpdateGithubPublicTest extends CloseOnUpdateBase
+class CloseOnUpdateGithubPublicTest extends CloseOnUpdateGithubTest
 {
-    public function testPrsClosedGithubPublic(&$retries = 0)
+    public function setUp()
     {
+        parent::setUp();
+        $this->token = getenv('GITHUB_PRIVATE_USER_TOKEN');
+        $this->url = getenv('GITHUB_PUBLIC_REPO');
+    }
+
+    protected function getExtraParams()
+    {
+        $extra_params = parent::getExtraParams();
         $project = new ProjectData();
         $project->setNid(getenv('GITHUB_PUBLIC_PROJECT_NID'));
-        // First make sure we have created PRs for all of them.
-        $extra_params = [
+        $extra_params = $extra_params + [
             'project' => sprintf("'%s'", json_encode(serialize($project))),
             'fork_to' => getenv('GITHUB_FORK_TO'),
             'token_url' => getenv('TOKEN_URL'),
-            'fork_user' => getenv('FORK_USER'),
-            'fork_mail' => getenv('FORK_MAIL'),
         ];
-        $json = $this->getProcessAndRunWithoutError(getenv('GITHUB_PRIVATE_USER_TOKEN'), getenv('GITHUB_PUBLIC_REPO'), $extra_params);
-        $closed_with_success = self::hasPrClosedAndPrClosedSuccess($json);
-        if ($retries < 20 && !$closed_with_success) {
-            $retries++;
-            return $this->testPrsClosedGithub($retries);
-        }
-        self::assertTrue($closed_with_success, 'PR was not both attempted and succeeded with being closed');
+        return $extra_params;
+    }
+
+    protected function createBranchName()
+    {
+        return 'psrlog101' . random_int(400, 999);
     }
 }
--- tests/Integration/CloseOnUpdateGithubTest.php
+++ tests/Integration/CloseOnUpdateGithubTest.php
@@ -2,19 +2,99 @@
 
 namespace Violinist\UpdateCheckRunner\Tests\Integration;
 
-use Stevenmaguire\OAuth2\Client\Provider\Bitbucket;
+use Github\Api\GitData;
+use Github\Api\PullRequest;
+use Github\Api\Repo;
+use Github\Client;
 use Violinist\Slug\Slug;
 
 class CloseOnUpdateGithubTest extends CloseOnUpdateBase
 {
+    protected $token;
+    protected $url;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->token = getenv('GITHUB_PRIVATE_USER_TOKEN');
+        $this->url = getenv('GITHUB_PRIVATE_REPO');
+    }
+
     public function testPrsClosedGithub(&$retries = 0)
     {
-        $json = $this->getProcessAndRunWithoutError(getenv('GITHUB_PRIVATE_USER_TOKEN'), getenv('GITHUB_PRIVATE_REPO'));
+        $client = new Client();
+        $token = $this->token;
+        $client->authenticate($token, null, Client::AUTH_HTTP_TOKEN);
+        $slug = Slug::createFromUrl($this->url);
+        /** @var Repo $repo */
+        $repo = $client->api('repo');
+        $info = $repo->show($slug->getUserName(), $slug->getUserRepo());
+        $default_branch = $info['default_branch'];
+        $branch = $repo->branches($slug->getUserName(), $slug->getUserRepo(), $default_branch);
+        $sha = $branch["commit"]["sha"];
+        /** @var GitData $api */
+        $api = $client->api('git');
+        $tree = [];
+        $data = $api->blobs()->create($slug->getUserName(), $slug->getUserRepo(), [
+            'content' => 'temp file',
+            'encoding' => 'utf-8',
+        ]);
+        $tree[] = [
+            'sha' => $data["sha"],
+            'mode' => '100644',
+            'type' => 'blob',
+            'path' => 'test.txt',
+        ];
+        $data = $api->trees()->create($slug->getUserName(), $slug->getUserRepo(), [
+            'tree' => $tree,
+            'base_tree' => $sha,
+            'parents' => [
+                $sha,
+            ],
+        ]);
+        $data = $api->commits()->create($slug->getUserName(), $slug->getUserRepo(), [
+            'message' => 'temp commit',
+            'tree' => $data["sha"],
+            'parents' => [
+                $sha,
+            ],
+        ]);
+        $branch_name = $this->createBranchName();
+        $data = $api->references()->create($slug->getUserName(), $slug->getUserRepo(), [
+            'ref' => 'refs/heads/' . $branch_name,
+            'sha' => $data['sha'],
+            'force' => true,
+        ]);
+        $user_name = $slug->getUserName();
+        $user_repo = $slug->getUserRepo();
+        /** @var PullRequest $prs */
+        $prs = $client->api('pull_request');
+        $data = $prs->create($user_name, $user_repo, [
+            'base'  => $default_branch,
+            'head'  => $branch_name,
+            'title' => 'test temp pr',
+            'body'  => 'test temp pr',
+        ]);
+        $extra_params = $this->getExtraParams();
+        $json = $this->getProcessAndRunWithoutError($token, $slug->getUrl(), $extra_params);
         $closed_with_success = self::hasPrClosedAndPrClosedSuccess($json);
         if ($retries < 20 && !$closed_with_success) {
             $retries++;
             return $this->testPrsClosedGithub($retries);
         }
         self::assertTrue($closed_with_success, 'PR was not both attempted and succeeded with being closed');
+    }
+
+    protected function createBranchName()
+    {
+        return 'psrlog100' . random_int(400, 999);
+    }
+
+    protected function getExtraParams()
+    {
+        return [
+            'fork_user' => getenv('FORK_USER'),
+            'fork_mail' => getenv('FORK_MAIL'),
+        ];
     }
 }
