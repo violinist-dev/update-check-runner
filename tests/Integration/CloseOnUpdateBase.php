dce3ae2a7b70b1d5bf5a3439f0c322d445fdeca2
Eirik Stanghelle Morland <eirik@morland.no>
2022-07-14 13:52:42 +0200

    Update CloseOnUpdateBase.php

--- tests/Integration/CloseOnUpdateBase.php
+++ tests/Integration/CloseOnUpdateBase.php
@@ -19,7 +19,7 @@ abstract class CloseOnUpdateBase extends IntegrationBase
         }
     }
 
-    public function setUp()
+    public function setUp() : void
     {
         parent::setUp();
         $this->branchName = $this->createBranchName();
