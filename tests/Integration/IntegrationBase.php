6774edcc869e469e2cf41d3b6ddeeef9cef052b5
Eirik Stanghelle Morland <eirik@morland.no>
2024-06-07 20:05:12 +0200

    Update IntegrationBase.php

--- tests/Integration/IntegrationBase.php
+++ tests/Integration/IntegrationBase.php
@@ -102,7 +102,7 @@ abstract class IntegrationBase extends TestCase
         $this->findMessage('composer install completed successfully', $json);
     }
 
-    protected function getProcessAndRunWithoutError($token, $url, $other_env = [])
+    protected function getProcessAndRun($token, $url, $other_env = [])
     {
         $command = [
             'docker',
@@ -121,6 +121,20 @@ abstract class IntegrationBase extends TestCase
         $command[] = 'update-check-runner';
         $process = new Process($command, null, null, null, 600);
         $process->run();
+        return $process;
+    }
+
+    protected function getProcessAndRunGetJson($token, $url, $other_env = [])
+    {
+        $process = $this->getProcessAndRun($token, $url, $other_env);
+        $json = @json_decode($process->getOutput());
+        $this->assertFalse(empty($json));
+        return $json;
+    }
+
+    protected function getProcessAndRunWithoutError($token, $url, $other_env = [])
+    {
+        $process = $this->getProcessAndRun($token, $url, $other_env);
         if ($process->getExitCode()) {
             var_export($process->getOutput());
         }
