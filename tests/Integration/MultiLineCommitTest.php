fdd1f689ae9718ed5a9819a7da7f00798ac245ce
Eirik Stanghelle Morland <eirik@morland.no>
2024-07-08 23:07:47 +0200

    Update MultiLineCommitTest.php

--- tests/Integration/MultiLineCommitTest.php
+++ tests/Integration/MultiLineCommitTest.php
@@ -9,6 +9,8 @@ class MultiLineCommitTest extends IntegrationBase
     {
         $json = $this->getProcessAndRunWithoutError($_SERVER['GITHUB_PRIVATE_USER_TOKEN'], $_SERVER['GITHUB_PRIVATE_REPO'], [
             'USE_NEW_COMMIT_MSG' => '1',
+            'fork_user' => $_SERVER['FORK_USER'],
+            'fork_mail' => $_SERVER['FORK_MAIL'],
         ]);
         $this->assertStandardOutput($_SERVER['GITHUB_PRIVATE_REPO'], $json);
         print_r($json);
