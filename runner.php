01e53b273bebc288cc3e596c8cccd84184b88c83
Eirik S. Morland <eirik@morland.no>
2024-08-19 20:19:02 +0200

    Trace

--- runner.php
+++ runner.php
@@ -127,7 +127,7 @@ try {
 }
 catch (Exception $e) {
     $output = $cosy->getOutput();
-    $output[] = new Message('Caught Exception: ' . $e->getMessage(), Message::ERROR);
+    $output[] = new Message('Caught Exception: ' . $e->getMessage() . ' with the stack trace ' . $e->getTraceAsString(), Message::ERROR);
     $code = 1;
 }
 $json = [];
