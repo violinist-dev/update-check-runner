6168a27e718f7453d899b5cf7d8f80e55b9c389a
Eirik Stanghelle Morland <eirik@morland.no>
2024-09-04 10:47:29 +0200

    Update LicenceTest.php

--- tests/Integration/LicenceTest.php
+++ tests/Integration/LicenceTest.php
@@ -27,10 +27,11 @@ class LicenceTest extends IntegrationBase
 
     public function testValidForWrongKey()
     {
-        $json = $this->getProcessAndRunWithoutError($_SERVER['GITHUB_PRIVATE_USER_TOKEN'], $_SERVER['GITHUB_PRIVATE_REPO'], [
+        $process = $this->getProcessAndRun($_SERVER['GITHUB_PRIVATE_USER_TOKEN'], $_SERVER['GITHUB_PRIVATE_REPO'], [
             'LICENCE_KEY' => 'fYtLakIxFEBdy1vB_SU3iaPrTRwVugFnj9AGxRYVsRSha-ju3m7qpFNHhwPn_C5vS38tDGW6jo_DOI7zZfcy5n6cu7_3ef8vU8HyfS6cyrR6Xq767XOcvqb1KKgoCKqo6_vyI02pWk6YgyU3gsrqgaS5pwcVo9aNY2AQbS1TZABJjwWRHCUqNrCK7pTd2TE6hA01rMQKTJUNmjlLjbYlYc4c3TQxS6iqYH8',
         ]);
-        $this->assertStandardOutput($_SERVER['GITHUB_PRIVATE_REPO'], $json);
+        $json = @json_decode($process->getOutput());
+        self::assertNotEmpty($json);
         $this->findMessage('Licence key is not valid for any of the known public keys.', $json);
     }
 
